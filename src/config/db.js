249e456cd5f658848e38d9b35213953a7d6dc922
joshua-fernandess <fernandessjosh@gmail.com>
2024-10-14 17:17:25 -0300

    a

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     mongoose
-        .connect('mongodb://localhost:27017/oficina')
+        .connect('mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}')
         .then(() => console.log('Connected to MongoDB'))
         .catch((error) => console.log(error));  
 };
