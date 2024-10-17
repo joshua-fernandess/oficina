fd0781607015b794c8c8e51bd7bb9214bda822a7
Joshua <128092569+joshua-fernandess@users.noreply.github.com>
2024-10-17 16:58:28 -0300

    atualizei db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     mongoose
-        .connect(`mongodb+srv://${process.env.URI}`)
+        .connect(`${process.env.URI}`)
         .then(() => console.log('Connected to MongoDB'))
         .catch((error) => console.log(error));  
 };
